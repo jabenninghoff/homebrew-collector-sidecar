69add56f9fd5e4a10b66f1213de40c5fd621fb28
jabenninghoff <jbenninghoff@mac.com>
2017-12-23 11:25:32 -0600

    Import changes from official Homebrew formula

--- collector-sidecar.rb
+++ collector-sidecar.rb
@@ -1,8 +1,8 @@
 class CollectorSidecar < Formula
   desc "Manage log collectors through Graylog"
   homepage "https://github.com/Graylog2/collector-sidecar"
-  url "https://github.com/Graylog2/collector-sidecar/archive/0.0.9.tar.gz"
-  sha256 "a854d84bf65e3fb9409743869701dab5c059d14d0b615acd14c8923bef30bc9f"
+  url "https://github.com/Graylog2/collector-sidecar/archive/0.1.4.tar.gz"
+  sha256 "3d73f8054a52411ff6d71634bc93b23a55372477069fcfad699876f82ae22ce8"
 
   depends_on "glide" => :build
   depends_on "go" => :build
@@ -13,19 +13,44 @@ class CollectorSidecar < Formula
     ENV["GOPATH"] = buildpath
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
     (buildpath/"src/github.com/Graylog2/collector-sidecar").install buildpath.children
+
     cd "src/github.com/Graylog2/collector-sidecar" do
       inreplace "main.go", "/etc", etc
-      inreplace "collector_sidecar.yml", "/usr", HOMEBREW_PREFIX
-      inreplace "collector_sidecar.yml", "/etc", etc
-      inreplace "collector_sidecar.yml", "/var", var
+
+      inreplace "collector_sidecar.yml" do |s|
+        s.gsub! "/usr", HOMEBREW_PREFIX
+        s.gsub! "/etc", etc
+        s.gsub! "/var", var
+      end
+
       system "glide", "install"
       system "make", "build"
-      (etc/"graylog/collector-sidecar").install "collector_sidecar.yml" unless (etc/"graylog/collector-sidecar/collector_sidecar.yml").exist?
+      (etc/"graylog/collector-sidecar").install "collector_sidecar.yml"
       bin.install "graylog-collector-sidecar"
+      prefix.install_metafiles
     end
   end
 
+  plist_options :manual => "graylog-collector-sidecar"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{opt_bin}/graylog-collector-sidecar</string>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+  EOS
+  end
+
   test do
-    system "#{bin}/graylog-collector-sidecar", "-version"
+    assert_match version.to_s, shell_output("#{bin}/graylog-collector-sidecar -version")
   end
 end
